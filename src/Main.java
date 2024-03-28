67adb2a109b1163e56bbed6a764713bd139cac1a
borisova <borisova.marie1@gmail.com>
2024-03-28 19:19:00 +0100

    Added a temporary draft of the story for the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Attending a party happening in an abandoned warehouse " +
+                "with complete strangers wasn't your best idea. It is currently 3:42 AM " +
+                "and you're in the seediest bathroom you've ever had the displeasure of being in. " +
+                "Seeing a mysterious bottle of pills on the counter, " +
+                "you decide what's the worst that could happen, pop open the lid and guzzle down all of the pills. " +
+                "Surprisingly, nothing happens. Unimpressed, you leave the bathroom. " +
+                "You notice a door down the hallway. Getting closer to it, you feel an ominous energy coming off of it. " +
+                "Being the danger seeker that you are, you want to know what or who is on the other side, " +
+                "on the other hand, this party has already filled you with insurmountable disappointment and you want to leave. " +
+                "The choice is yours");
+
     }
 }
