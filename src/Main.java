cb76f42da4d5522f194fca10b17abd8bb305507c
borisova <borisova.marie1@gmail.com>
2024-03-29 17:22:13 +0100

    Added temporary function for user input

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Attending a party happening in an abandoned warehouse " +
-                "with complete strangers wasn't your best idea. It is currently 3:42 AM " +
-                "and you're in the seediest bathroom you've ever had the displeasure of being in. " +
-                "Seeing a mysterious bottle of pills on the counter, " +
+                "with complete strangers wasn't your best idea.");
+        System.out.println("[Press ENTER to continue]");
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+
+        System.out.println("It is currently 3:42 AM " +
+        "and you're in the seediest bathroom you've ever had the displeasure of being in. ");
+        System.out.println("[Press ENTER to continue]");
+        sc.nextLine();
+
+        System.out.println("Seeing a mysterious bottle of pills on the counter, " +
                 "you decide what's the worst that could happen, pop open the lid and guzzle down all of the pills. " +
                 "Surprisingly, nothing happens. Unimpressed, you leave the bathroom. " +
                 "You notice a door down the hallway. Getting closer to it, you feel an ominous energy coming off of it. " +
                 "Being the danger seeker that you are, you want to know what or who is on the other side, " +
                 "on the other hand, this party has already filled you with insurmountable disappointment and you want to leave. " +
                 "The choice is yours");
+
 
     }
 }
