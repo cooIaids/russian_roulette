86345c36c8f2f562795370a35b5055c30f835be5
borisova <borisova.marie1@gmail.com>
2024-05-26 19:41:38 +0200

    Added test class GameStartTest for class GameStart

---
+++ src/GameStartTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameStartTest {
+
+    @org.junit.jupiter.api.Test
+    void playersChoiceForGameMode() {
+        int firstPlayerChoice;
+        int secondPlayerChoice;
+    }
+}
