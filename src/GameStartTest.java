4a0d7ad30b66cc812f71d88760374a60b7a3b1df
borisova <borisova.marie1@gmail.com>
2024-05-29 19:11:27 +0200

    Added test method for class GameStartTest, created a .jar file for the project

--- src/GameStartTest.java
+++ src/GameStartTest.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+//import static org.mockito.Mockito.mock;
 class GameStartTest {
 
     @org.junit.jupiter.api.Test
@@ -17,9 +17,9 @@ class GameStartTest {
         InputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(inputStream);
 
-        RevolverGameModeAI revolverGameModeAI = mock(RevolverGameModeAI.class);
+      /*  RevolverGameModeAI revolverGameModeAI = mock(RevolverGameModeAI.class);
         RevolverGameModePlayer revolverGameModePlayer = mock(RevolverGameModePlayer.class);
         ShotgunGameModePlayer shotgunGameModePlayer = mock(ShotgunGameModePlayer.class);
-        ShotgunGameModeAI shotgunGameModeAI = mock(ShotgunGameModeAI.class);
+        ShotgunGameModeAI shotgunGameModeAI = mock(ShotgunGameModeAI.class);*/
     }
 }
