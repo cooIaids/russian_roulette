1aa8b8fb608038c16c75aba25b7a66f4a2213946
marie <126610455+maru-bor@users.noreply.github.com>
2024-06-01 21:36:15 +0200

    Added comment

--- src/TestClasses/GameStartTest.java
+++ src/TestClasses/GameStartTest.java
@@ -10,6 +10,10 @@ import static org.mockito.Mockito.*;
 
 class GameStartTest {
 
+    /**
+    * Tests the method playersChoiceForGameMode()
+    */
+
     @org.junit.jupiter.api.Test
     void playersChoiceForGameMode() {
         String simulatedInput = "1 \n 1 \n";
